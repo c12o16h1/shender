79b3b017e6985cde58289ba1d22fb55cf05f5ad4
Yurii Kovalenko <ua.preston@gmail.com>
2018-10-24 22:43:04 +0700

    basic cached web server

--- pkg/webserver/server.go
+++ pkg/webserver/server.go
@@ -1,1 +1,13 @@
-package server
+package webserver
+
+import (
+	"github.com/yosssi/go-fileserver"
+	"net/http"
+	"fmt"
+)
+
+func Serve(dir string, port uint) error {
+	fs := fileserver.New(fileserver.Options{})
+	http.Handle("/", fs.Serve(http.Dir(dir)))
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
